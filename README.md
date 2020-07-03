c9d256b5d1d2f39d2e6b8feebbe74504cc7e550c
antegs <67729522+antegs@users.noreply.github.com>
2020-07-03 15:32:40 +0900

    Create script

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
 # syncseen
+A python script to sync played/unplayed status of media across Emby and/or Jellyfin instances.
+
+You will need the API key for the two servers which can be generated in the Admin section of each server.  This script can be used to sync two Emby servers, two Jellyfin servers, or an Emby and a Jellyfin server.
+
+## Basic operation
+The most basic operation will sync the played and unplayed status of all movies, tv series, and videos from the host server to the destination server for all users.  This assumes that both servers have the same media and users
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> -dhost=<destination URL> -dkey=<destination API key>
+```
+A user list can be passed to select only some users to sync. User names are case sensitive
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> -dhost=<destination URL> -dkey=<destination API key> -u Admin,Kids
+```
+
+## Find existing users
+To find the exact user names to use when syncing use the --listusers option
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> -dhost=<destination URL> -dkey=<destination API key> --listusers
+```
+This can be run for only the source server
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> --listusers
+```
+or only the destination server
+```
+python syncseen.py -dhost=<destination URL> -dkey=<destination API key> --listusers
+```
+
+## Write and read data from file
+For systems where the source server and the destination server cannot be accessed at the same time, it is possible to first save the played/unplayed statuses in a file and then read that file in a separate action
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> -o savedstatuses.txt
+python syncseen.py -dhost=<destination URL> -dkey=<destination API key> -i savedstatuses.txt
+```
+
+## Specify media types
+The default media types processed are movie, episode (for series), and video.  The "-t ITEMTYPES" option allows for finer control
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> -dhost=<destination URL> -dkey=<destination API key> -t movie,episode
+```
+
+## Choose sync strategy
+The default sync strategy is to update both played and unplayed items from the source server to the destination server, creating a mirror setup. But it is possible to update only the played items found on the source server
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> -dhost=<destination URL> -dkey=<destination API key> -s playedonly
+```
+or only the unplayed items found on the source server
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> -dhost=<destination URL> -dkey=<destination API key> -s unplayedonly
+```
+
+## Get some statistics about the media
+To find the total number of movies and tv series, as well as the number of played and unplayed items for each user, use the --stats option
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> -dhost=<destination URL> -dkey=<destination API key> --stats
+```
+This can be run for only the source or destination server
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> --stats
+python syncseen.py -dhost=<destination URL> -dkey=<destination API key> --stats
+```
+
+## Misc
+* Running the script without arguments shows the help text
+```
+python syncseen.py
+```
+```
+usage: syncseen.py [-h] [-np] [-shost SHOST] [-skey SKEY] [-dhost DHOST]
+                   [-dkey DKEY] [-o FILE] [-i FILE] [-u USERLIST]
+                   [-t ITEMTYPES] [-s {mirror,playedonly,unplayedonly}]
+                   [--listusers] [--stats]
+
+ optional arguments:
+  -h, --help            show this help message and exit
+  -np                   no progress output
+  -shost SHOST          source source URL
+  -skey SKEY            source api key
+  -dhost DHOST          destination source URL
+  -dkey DKEY            destination api key
+  -o FILE               write data to FILE
+  -i FILE               read data from file
+  -u USERLIST           comma seperated user list
+  -t ITEMTYPES          comma seperated list of item types
+  -s {mirror,playedonly,unplayedonly}
+                        sync strategy (mirror is default)
+  --listusers           only get the user list
+  --stats               print some stats
+```
+
+* Most operations show a progress indicator for every few media items. This can be suppressed with the -np argument
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> --stats -np
+```
+
+* Many options can be combined
+```
+python syncseen.py -shost=<source URL> -skey=<source API key> -u Kids -t movie -np -o savedstatuses.txt
+```
+
+* A concrete command would look something like this
+```
+python syncseen.py -shost="http://192.168.1.100:8096" -skey="5c500d1715374703946bb187d993d507" -dhost="http://192.168.1.101:8096" -dkey="7bbd4a7c76c84530888d86ef70642550" -u Kids
+```
+
+## Future
+* Currently the users have to be the same in both servers.  It might be helpful to add some functionality where users can be mapped between the two servers.  For now this is possible by saving a user to a file, and then using an text editor to rename all user instances, before importing to the destination server.
+
+* The next major Jellyfin release is planning an API overhaul which might make this tool unusable for Jellyfin instances.
---
+++ syncseen.py
@@ -0,0 +1,196 @@
+import requests
+import ast
+import sys
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-np", action="store_false",
+                    help="no progress output")
+parser.add_argument("-shost", dest="shost",
+                    help="source host url")
+parser.add_argument("-skey", dest="skey",
+                    help="source api key")
+parser.add_argument("-dhost", dest="dhost",
+                    help="destination host url")
+parser.add_argument("-dkey", dest="dkey",
+                    help="destination api key")
+parser.add_argument("-o", dest="outfile",
+                    help="write data to FILE", metavar="FILE")
+parser.add_argument("-i", dest="infile",
+                    help="read data from file", metavar="FILE")
+parser.add_argument("-u", dest="userlist",
+                    help="comma seperated user list")
+parser.add_argument("-t", dest="itemtypes",
+                    help="comma seperated list of item types")
+parser.add_argument("-s", choices=['mirror', 'playedonly', 'unplayedonly'], default='mirror',
+                    help="sync strategy (mirror is default)")
+parser.add_argument("--listusers", action="store_true",
+                    help="only get the user list")
+parser.add_argument("--stats", action="store_true",
+                    help="print some stats")
+args = parser.parse_args()
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    quit()
+
+nosource = True;
+if (args.shost != None and args.skey != None):
+    nosource = False;
+nodest = True;
+if (args.dhost != None and args.dkey != None):
+    nodest = False;
+
+if nosource and nodest:
+    print("Need at least source host and key OR destination host and key.")
+    quit()
+
+if args.outfile != None and nosource:
+    print("Source host and key are needed to create output file.")
+    quit()
+if args.infile != None and nodest:
+    print("Destination host and key are needed to read input file.")
+    quit()
+
+itemTypes="movie,episode,video,musicvideo"
+if args.itemtypes != None:
+    itemTypes = args.itemtypes
+
+usersRequested = None
+if args.userlist != None:
+    usersRequested = args.userlist.split(",")
+
+if args.outfile != None and args.infile != None:
+    print("Cannot use both input and output file.")
+    quit()
+
+def update(key, userid, itemid):
+    if key in loadedusers:
+        URL = args.dhost+"/Users/"+userid+"/PlayedItems/"+itemid+"?api_key="+args.dkey
+        played = loadedusers[key]
+        if played and (args.s == "mirror" or args.s == "playedonly"):
+            # make post request (played)
+            PARAMS = {'DatePlayed':20200101000000}
+            r = requests.post(url = URL, params = {})
+            if r.status_code != 200:
+                print(r)
+        if not played and (args.s == "mirror" or args.s == "unplayedonly"):
+            # make delete request (not played)
+            r = requests.delete(url = URL, params = {})
+            if r.status_code != 200:
+                print(r)
+    else:
+        print("Item not in source: %s"%(key))
+
+def process(calltype, callhost, callkey):
+    URL = callhost+"/Users?api_key="+callkey
+    r = requests.get(url = URL, params = {})
+    usersdb = r.json()
+    users={}
+
+    if args.listusers:
+        print("--- " + calltype + " Users:")
+        for usr in usersdb:
+            print(usr['Name'])
+    else:
+        URL = callhost+"/Library/PhysicalPaths?api_key="+callkey
+        r = requests.get(url = URL, params = {})
+        pathsdb = r.json()
+
+        userCount = 0
+        movieCount = 0
+        episodeCount = 0
+        videoCount = 0
+        playedList = {}
+
+        for usr in usersdb:
+            userid = usr['Id']
+            username = usr['Name']
+            if usersRequested == None or username in usersRequested:
+                userCount += 1
+                if userCount == 1:
+                    print("--- Processing " + calltype)
+
+                startindex = 0
+                itemlimit = 100
+                keepgoing = True
+                while keepgoing:
+                    if args.np:
+                        print(username + " " + str(startindex))
+                    URL = callhost+"/Users/"+userid+"/Items?IncludeItemTypes="+itemTypes+"&Recursive=true&StartIndex="+str(startindex)+"&Limit="+str(itemlimit)+"&api_key="+callkey
+                    r = requests.get(url = URL, params = {})
+                    itemsdb = r.json()
+
+                    if len(itemsdb['Items']) < itemlimit:
+                        keepgoing = False
+                    else:
+                        startindex += itemlimit
+                        keepgoing = True
+
+                    for item in itemsdb['Items']:
+                        itemid = item['Id']
+                        itemplayed = item['UserData']['Played']
+
+                        URL = callhost+"/Users/"+userid+"/Items/"+itemid+"?api_key="+callkey
+                        r = requests.get(url = URL, params = {})
+                        itemdb = r.json()
+
+                        if itemdb['MediaSources'][0]['Type'] == "Default":
+                            if item['Type'] == "Episode":
+                                episodeCount += 1
+                                key = username + ':' + item['Type']  + ':' + str(itemplayed)
+                            elif item['Type'] == "Movie":
+                                movieCount += 1
+                                key = username + ':' + item['Type'] + ':' + str(itemplayed)
+                            elif item['Type'] == "Video" or item['Type'] == "MusicVideo":
+                                videoCount += 1
+                                key = username + ':Video:' + str(itemplayed)
+                            if key in playedList:
+                                playedList[key] = playedList[key]+1
+                            else:
+                                playedList[key] = 1
+
+                            if not args.stats:
+                                itempath = itemdb['Path']
+                                itempathbefore = itempath
+                                for path in pathsdb:
+                                    if itempath.startswith(path) and itempath[len(path):len(path)+1] == "/":
+                                        itempath = itempath[len(path)+1:]
+                                        exit
+                                if itempathbefore == itempath:
+                                    print("Paths not corrected.")
+                                    continue
+
+                                key = username + ':' + itempath
+                                if calltype == "Source":
+                                    if key in users:
+                                        print("Key already in users: " + key)
+                                    else:
+                                        users[key] = itemplayed
+                                else:
+                                    update(key, userid, itemid)
+
+        if calltype == "Source" and args.outfile != None:
+            file = open(args.outfile, 'w')
+            file.write(str(users))
+            file.close()
+            quit()
+
+        if args.stats:
+            print("--- " + calltype + " stats:")
+            if userCount != 0:
+                print("Users: %s\nTotal Movies: %s\nTotal Episodes: %s\nTotal Videos: %s"%(
+                userCount,movieCount,episodeCount,videoCount))
+            for key, value in sorted(playedList.items()):
+                print("%s: %s"%(key.replace(":False", ":NotPlayed").replace(":True", ":Played"), value))
+        return users
+
+if args.infile == None:
+    if not nosource:
+        loadedusers = process("Source", args.shost, args.skey)
+else:
+    with open(args.infile, 'r') as content_file:
+        loadedusers = ast.literal_eval(content_file.read().strip())
+
+if not nodest:
+    process("Destination", args.dhost, args.dkey)
